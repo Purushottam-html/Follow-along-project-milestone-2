d65e648da3a2e071da930ec625565066d927fb27
Purushottam <pallipurushotham333@gmail.com>
2025-03-24 18:47:25 +0530

    updated vite config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows access from external networks
+    port: 5173, // Ensure it matches your logs
+    strictPort: true, // Ensures the app uses the defined port
+    allowedHosts: ['follow-along-project-milestone-2.onrender.com'], // Allow your Render domain
+  }
 })
